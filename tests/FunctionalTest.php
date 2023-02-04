18c0a7e5c0d85db79a358380f077e0c88a06f497
Baptiste Langlade <baptouuuu@gmail.com>
2023-02-04 16:44:48 +0100

    add async filesystem test

---
+++ tests/FunctionalTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Async\OperatingSystem;
+
+use Innmind\Async\OperatingSystem\Factory;
+use Innmind\OperatingSystem\Factory as Sync;
+use Innmind\Filesystem\Name;
+use Innmind\Url\Path;
+use Innmind\Mantle\{
+    Forerunner,
+    Source\Predetermined,
+};
+use PHPUnit\Framework\TestCase;
+
+class FunctionalTest extends TestCase
+{
+    private $factory;
+    private $synchronous;
+
+    public function setUp(): void
+    {
+        $this->synchronous = Sync::build();
+        $this->factory = Factory::of($this->synchronous);
+    }
+
+    public function testAsyncFilesystem()
+    {
+        $jsonFinishedFirst = null;
+        $total = 0;
+        $forerunner = Forerunner::of($this->synchronous->clock());
+        $forerunner(null, Predetermined::of(
+            function($suspend) use (&$jsonFinishedFirst, &$total) {
+                $this
+                    ->factory
+                    ->build($suspend)
+                    ->filesystem()
+                    ->mount(Path::of('./'))
+                    ->get(Name::of('composer.lock'))
+                    ->match(
+                        static fn($file) => $file,
+                        static fn() => throw new \LogicException('file not found'),
+                    )
+                    ->content()
+                    ->lines()
+                    ->foreach(static function() use (&$total) {
+                        ++$total;
+                    });
+                $this->assertTrue($jsonFinishedFirst);
+            },
+            function($suspend) use (&$jsonFinishedFirst, &$total) {
+                $this
+                    ->factory
+                    ->build($suspend)
+                    ->filesystem()
+                    ->mount(Path::of('./'))
+                    ->get(Name::of('composer.json'))
+                    ->match(
+                        static fn($file) => $file,
+                        static fn() => throw new \LogicException('file not found'),
+                    )
+                    ->content()
+                    ->lines()
+                    ->foreach(static function() use (&$total) {
+                        ++$total;
+                    });
+                $jsonFinishedFirst = true;
+            },
+        ));
+
+        $this->assertSame(6645, $total);
+    }
+}

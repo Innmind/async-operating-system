d7848bc5da97856d95b55ebacddd7f4279e42a9f
Baptiste Langlade <baptouuuu@gmail.com>
2023-02-04 16:32:11 +0100

    add os factory

---
+++ src/Factory.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Async\OperatingSystem;
+
+use Innmind\OperatingSystem\{
+    OperatingSystem,
+    Config,
+};
+use Innmind\Mantle\Suspend;
+
+final class Factory
+{
+    private OperatingSystem $synchronous;
+    private ?Config $config;
+
+    private function __construct(OperatingSystem $synchronous, ?Config $config)
+    {
+        $this->synchronous = $synchronous;
+        $this->config = $config;
+    }
+
+    /**
+     * @psalm-suppress PossiblyUnusedMethod
+     */
+    public static function of(OperatingSystem $synchronous, Config $config = null): self
+    {
+        return new self($synchronous, $config);
+    }
+
+    /**
+     * @psalm-suppress PossiblyUnusedMethod
+     */
+    public function build(Suspend $suspend): OperatingSystem
+    {
+        return Async::of(
+            $this->synchronous,
+            $suspend,
+            $this->config,
+        );
+    }
+}
